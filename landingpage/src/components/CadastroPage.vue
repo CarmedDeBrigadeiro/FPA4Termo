40f2438060d842b953c15a826777f3f6dcbf0d5e
Amanda <amanda.silva@ciag.org.br>
2024-11-02 21:51:47 -0300

    add auth service

--- landingpage/src/components/CadastroPage.vue
+++ landingpage/src/components/CadastroPage.vue
@@ -53,17 +53,49 @@
             <input type="text" id="cpf" v-model="form.cpf" @input="formatCPF" placeholder="000.000.000-00" />
           </div>
         </div>
-        
+
         <div class="address-data">ADRESS DATA</div>
-        <div class="address-1>">ADRESS 1 </div>
-        <div class="form-group">
-          <label for="postalCode">Postal Code (ZIP Code)</label>
-          <input type="text" id="postalCode" v-model="form.postalCode" />
+        <h2>Address Data</h2>
+        <div v-for="(address, index) in addresses" :key="index" class="address-form">
+          <div class="form-group">
+            <label for="postalCode">Postal Code (ZIP Code)</label>
+            <input type="text" v-model="address.postalCode" @blur="fetchAddressByPostalCode(address)" />
+          </div>
+          <div class="form-group">
+            <label for="street">Street</label>
+            <input type="text" v-model="address.street" />
+          </div>
+          <div class="form-group">
+            <label for="neighborhood">Neighborhood</label>
+            <input type="text" v-model="address.neighborhood" placeholder="Enter neighborhood" />
+          </div>
+          <div class="form-group">
+            <label for="addressType">Type of Address</label>
+            <select v-model="address.addressType">
+              <option value="residential">Residential</option>
+              <option value="commercial">Commercial</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="city">City</label>
+            <input type="text" v-model="address.city" placeholder="Enter city" />
+          </div>
+          <div class="form-group">
+            <label for="state">State</label>
+            <input type="text" v-model="address.state" placeholder="Enter state" />
+          </div>
+          <div class="form-group">
+            <label for="number">Number</label>
+            <input type="text" v-model="address.number" placeholder="Apartment/Suite" />
+          </div>
+          <div class="form-group">
+            <label for="additionalInfo">Additional Information</label>
+            <input type="text" v-model="address.additionalInfo" placeholder="Additional information" />
+          </div>
+          <button type="button" @click="removeAddress(index)">Remove Address</button>
         </div>
-        <div class="form-group">
-          <label for="street">Street</label>
-          <input type="text" id="street" v-model="form.street" />
-        </div>
+        <button type="button" @click="addAddress">Add Another Address</button>
+
         <button type="submit">Sign Up</button>
       </form>
     </div>
@@ -128,6 +160,40 @@ export default {
       this.addresses.splice(index, 1);
     },
   },
+  async fetchAddressByPostalCode(address) {
+    const cep = address.postalCode.replace(/\D/g, '');
+    if (cep.length === 8) {
+      try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const data = await response.json();
+        if (!data.erro) {
+          address.street = data.logradouro;
+          address.neighborhood = data.bairro;
+          address.city = data.localidade;
+          address.state = data.uf;
+        } else {
+          alert('Postal code not found');
+        }
+      } catch (error) {
+        alert('Error fetching postal code');
+      }
+    }
+  },
+  addAddress() {
+    this.addresses.push({
+      postalCode: '',
+      street: '',
+      neighborhood: '',
+      addressType: 'residential',
+      city: '',
+      state: '',
+      number: '',
+      additionalInfo: '',
+    });
+  },
+  removeAddress(index) {
+    this.addresses.splice(index, 1);
+  },
 };
 </script>
 
@@ -158,9 +224,9 @@ export default {
   float: left;
   margin-right: 500px;
   position: sticky;
-  top: 0; 
+  top: 0;
   margin-top: -200px;
-  
+
 }
 
 .image-section h2 {
@@ -181,7 +247,7 @@ h2 {
 
 .form-group {
   margin-bottom: 15px;
-  
+
 }
 
 label {
@@ -191,7 +257,8 @@ label {
   font-weight: bold;
 }
 
-input, select {
+input,
+select {
   width: 100%;
   padding: 10px;
   border: 1px solid #ccc;
@@ -215,7 +282,7 @@ input, select {
   color: #969696;
 }
 
-.personal{
+.personal {
   color: #969696;
   text-align: left;
   font-family: "Inter-Bold", sans-serif;
@@ -223,10 +290,10 @@ input, select {
   line-height: 150%;
   font-weight: 700;
   position: relative;
-  
+
 }
 
-.address-data{
+.address-data {
 
   color: #969696;
   text-align: left;
@@ -239,7 +306,7 @@ input, select {
   margin-top: 20px;
 }
 
-.address-1{
+.address-1 {
   color: #969696;
   text-align: left;
   font-family: "Inter-Bold", sans-serif;
@@ -248,7 +315,7 @@ input, select {
   font-weight: 700;
   position: relative;
   align-self: stretch;
-  
+
 }
 
 button {
@@ -269,9 +336,25 @@ button:hover {
 
 .form-group-inline {
   display: flex;
-  gap: 15px; 
+  gap: 15px;
 }
+
 .form-group-inline .form-group {
-  flex: 1; 
+  flex: 1;
 }
+
+#addAddress button {
+  display: flex;
+  height: 54px;
+  padding: 16px 8px;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  align-self: stretch;
+  border-radius: 10px;
+  border: 1px solid #000;
+}
+
+
+
 </style>
